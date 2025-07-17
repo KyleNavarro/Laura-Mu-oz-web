fffac7d1685f60f6db33d637730d102a458d83e2
kyle <kyle.y.navarro.j@gmail.com>
2025-07-16 20:48:25 -0500

    agregamos logica de botones para las cards y despliegue

--- js/readPersonal.js
+++ js/readPersonal.js
@@ -1,12 +1,46 @@
 const boton = document.querySelectorAll('.btnPersonal');
-const parrafosPersonales = document.querySelectorAll('.card-content p');
+const parrafos = document.querySelectorAll('.card-content p');
+const imagenes = document.querySelectorAll('.card2 img');
 
 boton.forEach((btn, index) => {
     btn.onclick = function(e){
         e.preventDefault();
 
-        const parrafoPersonal = parrafosPersonales[index];
-        parrafoPersonal.classList.toggle('paragraphPersonal');
-        boton[index].textContent =  parrafoPersonal.classList.contains('paragraphPersonal') ? 'Leer más' : 'Leer menos';
+        const parrafo = parrafos[index];
+        const imagen = imagenes[index];
+        parrafo.classList.toggle('paragraphPersonal');
+
+        const parrafoOculto = parrafo.classList.contains('paragraphPersonal');
+
+        btn.textContent = parrafoOculto ? 'Leer más' : 'Leer menos';
+        imagen.style.display = parrafoOculto ? 'block' : 'none';
+        
+    };
+});
+
+
+
+
+const cards = document.querySelectorAll('.card2');
+const toggleBtn = document.getElementById('toggleCards');
+
+// Oculta todas las cards después de la cuarta
+cards.forEach((card, index) => {
+    if (index >= 4) {
+        card.classList.add('hidden');
     }
+});
+
+toggleBtn.addEventListener('click', () => {
+    const hiddenCards = document.querySelectorAll('.card2.hidden');
+    const isShowing = hiddenCards.length === 0;
+
+    cards.forEach((card, index) => {
+        if (index >= 4) {
+            card.classList.toggle('hidden');
+            card.classList.toggle('show');
+        }
+    });
+
+    toggleBtn.textContent = isShowing ? 'Ver más' : 'Ver menos';
 });
